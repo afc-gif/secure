7e720290a2368e70715fc8c57e8e2a695185e887
DjCeejay <lordceejay12@gmail.com>
2026-05-29 10:58:07 +0100

    Fix: Parse manifest.json directly to load CSS and JS assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,18 +7,19 @@
     <title>{{ config('app.name', 'CCA Portal') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
-    <!-- Debug: Check if manifest exists -->
-    @if(file_exists(public_path('build/manifest.json')))
-        <meta name="debug" content="manifest-exists">
-        <!-- Try @vite() -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-        <meta name="debug" content="manifest-missing">
-        <link rel="stylesheet" href="/build/assets/app-NGirFLSX.css">
-        <script src="/build/assets/app-DO2nEFzp.js"></script>
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        $cssFile = $manifest['resources/css/app.css']['file'] ?? null;
+        $jsFile = $manifest['resources/js/app.js']['file'] ?? null;
+    @endphp
+    @if($cssFile)
+        <link rel="stylesheet" href="/build/{{ $cssFile }}">
     @endif
 </head>
 <body class="min-h-screen font-sans">
     {{ $slot }}
+    @if($jsFile)
+        <script src="/build/{{ $jsFile }}"></script>
+    @endif
 </body>
 </html>
