83e07e6b5c973ac9147858ba632d000d2cac768e
DjCeejay <lordceejay12@gmail.com>
2026-06-06 21:40:27 +0100

    Change in logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,5 @@
-<svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <rect width="64" height="64" rx="14" fill="#08090b"/>
-    <path d="M15 43c11-3 18-10 22-22 5 6 9 13 12 22H15Z" fill="#d8bf7a"/>
-    <path d="M20 43c9-6 16-11 26-14" stroke="#f4e4aa" stroke-width="4" stroke-linecap="round"/>
-    <path d="M15 48h34" stroke="#f8fafc" stroke-opacity=".82" stroke-width="3" stroke-linecap="round"/>
-</svg>
+<img
+    src="{{ asset('hey.png') }}"
+    alt="{{ config('app.name', 'CCA Portal') }} logo"
+    {{ $attributes->merge(['class' => 'block rounded-lg bg-white object-contain p-1 shadow-sm ring-1 ring-black/10']) }}
+>
